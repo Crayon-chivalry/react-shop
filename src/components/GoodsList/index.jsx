5750b4e4b7337e85e8dcd38f8053c085140aa097
123 <1193838563@qq.com>
2025-06-22 20:48:19 +0800

    init

--- src/components/GoodsList/index.jsx
+++ src/components/GoodsList/index.jsx
@@ -1,5 +1,21 @@
-const GoodsList = () => {
-  return <>GoodsList</>
+import './index.css'
+
+import Masonry from 'react-masonry-css'
+
+const GoodsList = ({ list }) => {
+  return (
+    <Masonry breakpointCols={2} className="my-masonry-grid">
+      {list.map(item =>
+        <div className="goods-item" key={item.id}>
+          <img src={item.cover} className="goods-cover" />
+          <div className="goods-content">
+            <div className="goods-name">{item.name}</div>
+            <div className="goods-price">￥<span>{item.price}</span></div>
+          </div>
+        </div>
+      )}
+    </Masonry>
+  )
 }
 
 export default GoodsList
