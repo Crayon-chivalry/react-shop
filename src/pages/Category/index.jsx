e67ea3ad50d52b8fd4582406cd4d45e502a0edbe
123 <1193838563@qq.com>
2025-06-22 22:03:37 +0800

    init

--- src/pages/Category/index.jsx
+++ src/pages/Category/index.jsx
@@ -1,5 +1,31 @@
-const Category = () => {
-  return <>Category</>
-}
+import './index.css'
+import { SearchBar, SideBar } from 'antd-mobile'
 
-export default Category
+const tabs = [
+  {id: 1, title: '服饰箱包'},
+  {id: 2, title: '食品'},
+  {id: 3, title: '电器'},
+  {id: 4, title: '手机'},
+]
+
+const Category = () => {
+  return (
+    <div className="category">
+      <div className='category-header'>
+        <SearchBar placeholder="请输入内容" className="search" />
+      </div>
+      
+
+      <div className='category-row'>
+        <SideBar>
+          {tabs.map((item) => (
+            <SideBar.Item key={item.id} title={item.title} />
+          ))}
+        </SideBar>
+        <div className="content">1</div>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 import { SearchBar, Swiper } from "antd-mobile";
 import { ScanningOutline, BellOutline } from "antd-mobile-icons";
 
-import GoodsList from "@/components/GoodsList"
+import GoodsList from "@/components/GoodsList";
 
 const appList = [
   { name: "海外购", image: "/src/assets/images/home-grid1.png" },
@@ -14,50 +14,73 @@ const appList = [
 ];
 
 const goodsList = [
-  { id: 1, name: 'SK-II神仙水精华液面霜保湿紧致护肤礼盒礼物', price: 2448, cover: '/src/assets/images/goods/SK-II.jpg' },
-  { id: 2, name: '心相印茶语抽纸整箱40包', price: 26.91, cover: '/src/assets/images/goods/zhijin.jpg' },
-  { id: 3, name: '豆本豆唯甄原味豆奶250ml*24盒营养早餐奶多口味植物蛋白饮品整箱', price: 29.31, cover: '/src/assets/images/goods/dounai.jpg' },
-  { id: 4, name: '海信空调易省电ProE370大3匹一级变频家用客厅立式柜机', price: 3569.06, cover: '/src/assets/images/goods/haixin.jpg' },
-  { id: 5, name: 'OPPO一加 Ace 5 新品学生游戏性能手机第三代骁龙 8', price: 1982.7, cover: '/src/assets/images/goods/ace5.jpg' },
-]
+  {
+    id: 1,
+    name: "SK-II神仙水精华液面霜保湿紧致护肤礼盒礼物",
+    price: 2448,
+    cover: "/src/assets/images/goods/SK-II.jpg",
+  },
+  {
+    id: 2,
+    name: "心相印茶语抽纸整箱",
+    price: 26.91,
+    cover: "/src/assets/images/goods/zhijin.jpg",
+  },
+  {
+    id: 3,
+    name: "豆本豆唯甄原味豆奶250ml*24盒营养早餐奶多口味植物蛋白饮品整箱",
+    price: 29.31,
+    cover: "/src/assets/images/goods/dounai.jpg",
+  },
+  {
+    id: 4,
+    name: "海信空调易省电ProE370大3匹一级变频家用客厅立式柜机",
+    price: 3569.06,
+    cover: "/src/assets/images/goods/haixin.jpg",
+  },
+  {
+    id: 5,
+    name: "OPPO一加 Ace 5 新品学生游戏性能手机第三代骁龙 8",
+    price: 1982.7,
+    cover: "/src/assets/images/goods/ace5.jpg",
+  },
+];
 
 const Home = () => {
   return (
     <div className="page">
-      <div className="white-bg">
-        <div className="header">
-          <div className="header-row">
-            <ScanningOutline color="#fff" />
-            <SearchBar placeholder="请输入内容" className="search" />
-            <BellOutline color="#fff" />
-          </div>
+      <div className="header">
+        <div className="header-row">
+          <ScanningOutline color="#fff" />
+          <SearchBar placeholder="请输入内容" className="search" />
+          <BellOutline color="#fff" />
         </div>
+      </div>
 
-        <Swiper indicatorProps={{ color: "white" }} className="swiper">
-          <Swiper.Item>
-            <img src="/src/assets/images/home-swiper.png" className="swiper-image" />
-          </Swiper.Item>
-        </Swiper>
+      <Swiper indicatorProps={{ color: "white" }} className="swiper">
+        <Swiper.Item>
+          <img src="/src/assets/images/home-swiper.png" className="swiper-image" />
+        </Swiper.Item>
+      </Swiper>
 
-        <div className="app-list">
-          {appList.map((item, index) => (
-            <div className="app-item" key={index}>
-              <img src={item.image} className="app-image" />
-              <div>{item.name}</div>
-            </div>
-          ))}
+      <div className="app-list">
+        {appList.map((item, index) => (
+          <div className="app-item" key={index}>
+            <img src={item.image} className="app-image" />
+            <div>{item.name}</div>
+          </div>
+        ))}
+      </div>
+
+      <img src="/src/assets/images/home-banner.png" className="banner" />
+
+      <div className="banner-grid">
+        <div className="banner-grid-item">
+          <img src="/src/assets/images/banner-grid-left.png" className="banner-grid-img" />
         </div>
-
-        <img src="/src/assets/images/home-banner.png" className="banner" />
-
-        <div className="banner-grid">
-          <div className="banner-grid-item">
-            <img src="/src/assets/images/banner-grid-left.png" className="banner-grid-img" />
-          </div>
-          <div className="banner-grid-item">
-            <img src="/src/assets/images/banner-grid-right1.png" className="banner-grid-img" />
-            <img src="/src/assets/images/banner-grid-right2.png" className="banner-grid-img" />
-          </div>
+        <div className="banner-grid-item">
+          <img src="/src/assets/images/banner-grid-right1.png" className="banner-grid-img" />
+          <img src="/src/assets/images/banner-grid-right2.png" className="banner-grid-img" />
         </div>
       </div>
 
